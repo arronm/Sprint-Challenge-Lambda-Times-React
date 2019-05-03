db0cd0342f985cf41b6e0335cc87ee91617ca9cc
Arron M <marshall.arron@gmail.com>
2019-05-03 09:30:00 -0700

    Stretch: Refactor Content to use styled components

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
-
+import styled from 'styled-components';
 import Tabs from './Tabs';
 import Cards from './Cards';
 
 // Importing our tab and card data. No need to change anything here.
 import { tabData, cardData } from '../../data';
+
+const StyledContent = styled.div`
+  display: flex; 
+  flex-direction: column; 
+  align-items: center;
+`;
 
 export default class Content extends Component {
   constructor(props) {
@@ -41,14 +47,14 @@ export default class Content extends Component {
   render() {
     console.log(this.state)
     return (
-      <div className="content-container">
+      <StyledContent>
         <Tabs
           tabs={this.state.tabs}
           selectedTab={this.state.selected}
           selectTabHandler={this.changeSelected}
         />
         <Cards cards={this.filterCards()} />
-      </div>
+      </StyledContent>
     );
   }
 }
