5b7e3d093a2b4ebd4359cb1832c68b8a22c4559f
Arron M <marshall.arron@gmail.com>
2019-05-03 08:12:19 -0700

    Update Cards/Card component(s) to use prop types

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import Card from './Card';
 
 const Cards = props => {
@@ -11,6 +12,9 @@ const Cards = props => {
   )
 }
 
-// Make sure you include prop types for all of your incoming props
+// TODO: Update our object shape here later
+Cards.propTypes = {
+  cards: PropTypes.arrayOf(PropTypes.object),
+}
 
 export default Cards;
