ff3288382a7586162b75be7c91bbda71311e78ae
Arron M <marshall.arron@gmail.com>
2019-05-03 09:35:51 -0700

    Stretch: refactor Cards component to use styled components

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import Card from './Card';
+
+const StyledCards = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: none;
+  flex-direction: row;
+  width: 100%;
+  margin-top: 16px;
+  flex-wrap: wrap;
+
+  @media (min-width: 1200px) {
+    width: 1200px;
+  }
+`;
 
 const Cards = props => {
   return (
-    <div className="cards-container">
+    <StyledCards>
       {
         props.cards.map(card => <Card {...card} />)
       }
-    </div>
+    </StyledCards>
   )
 }
 
