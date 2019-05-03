8b381cd55fef5d25a1c9dfb24d5dae1165c7654a
Arron M <marshall.arron@gmail.com>
2019-05-03 09:34:19 -0700

    Stretch: refactor Card component to use styled components

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -1,17 +1,57 @@
 import React from 'react';
+import styled from 'styled-components';
 import PropTypes from 'prop-types';
+
+const StyledCard = styled.div`
+  border-bottom: 1px solid lightgrey;
+  display: flex;
+  justify-content: space-between;
+  align-items: none;
+  flex-direction: column;
+  background-color: #fff;
+  width: 380px;
+  margin-bottom: 16px;
+  padding: 24px;
+`;
+const Headline = styled.div`
+  font-size: 25px;
+  font-family: Didot, serif;
+`;
+const Author = styled.div`
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+  margin-top: 15px;
+
+  span {
+    padding-left: 10px;
+    font-size: 12px;
+    letter-spacing: 1px;
+    font-weight: bold;
+  }
+`;
+const ImageContainer = styled.div`
+  padding-right: 10px;
+  border-right: 1px solid lightgrey;
+  height: 40px;
+
+  img {
+    width: 40px;
+  }
+`;
 
 const Card = props => {
   return (
-    <div className="card">
-      <div className="headline">{props.headline}</div>
-      <div className="author">
-        <div className="img-container">
+    <StyledCard>
+      <Headline>{props.headline}</Headline>
+      <Author>
+        <ImageContainer>
           <img src={props.img} alt={`by ${props.author}`} />
-        </div>
+        </ImageContainer>
         <span>By {props.author}</span>
-      </div>
-    </div>
+      </Author>
+    </StyledCard>
   );
 };
 
